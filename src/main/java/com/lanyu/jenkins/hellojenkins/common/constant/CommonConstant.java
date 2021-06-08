83f8e7a03c80abb320e2bf5400c0be7b2c96b533
互联网_兰瑜 <lanyu@bsoft.com.cn>
2021-06-08 20:37:58 +0800

    User实体类提交

--- src/main/java/com/lanyu/jenkins/hellojenkins/common/constant/CommonConstant.java
+++ src/main/java/com/lanyu/jenkins/hellojenkins/common/constant/CommonConstant.java
@@ -10,4 +10,24 @@ public interface CommonConstant {
      * 正常状态
      */
     Integer STATUS_NORMAL = 0;
+
+    /**
+     * 用户默认头像
+     */
+    String USER_DEFAULT_AVATAR = "https://ooo.0o0.ooo/2019/04/28/5cc5a71a6e3b6.png";
+
+    /**
+     * 用户正常状态
+     */
+    Integer USER_STATUS_NORMAL = 0;
+
+    /**
+     * 用户禁用状态
+     */
+    Integer USER_STATUS_LOCK = -1;
+
+    /**
+     * 普通用户
+     */
+    Integer USER_TYPE_NORMAL = 0;
 }
---
+++ src/main/java/com/lanyu/jenkins/hellojenkins/module/base/vo/RoleDTO.java
@@ -0,0 +1,8 @@
+package com.lanyu.jenkins.hellojenkins.module.base.vo;
+
+/**
+ * @author lanyu
+ * @date 2021年06月08日 20:35
+ */
+public class RoleDTO {
+}
